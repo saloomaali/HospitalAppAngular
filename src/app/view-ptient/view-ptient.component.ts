b3a6305389e04a11fdfda82b2cb892e6c9316a33
Salooma <saloomaali2000@gmail.com>
2022-11-24 12:57:03 +0530

    view component added

---
+++ src/app/view-ptient/view-ptient.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-ptient',
+  templateUrl: './view-ptient.component.html',
+  styleUrls: ['./view-ptient.component.css']
+})
+export class ViewPtientComponent {
+
+}
